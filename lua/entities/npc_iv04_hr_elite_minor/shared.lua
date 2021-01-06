6357e7c552b16a1dad9c371adfcb593d0633a5b6
ivan04 <ivanvladimir0435@gmail.com>
2021-01-05 20:29:12 -0700

    Added weapons to elite minor

--- lua/entities/npc_iv04_hr_elite_minor/shared.lua
+++ lua/entities/npc_iv04_hr_elite_minor/shared.lua
@@ -6,6 +6,12 @@ ENT.Models = {"models/halo_reach/characters/covenant/elite_minor.mdl"}
 
 ENT.CovRank = 1
 
+ENT.StartWeapons = {
+	[1] = "astw2_haloreach_plasma_rifle",
+	[2] = "astw2_haloreach_plasma_repeater",
+	[3] = "astw2_haloreach_needler"
+}
+
 list.Set( "NPC", "npc_iv04_hr_elite_minor", {
 	Name = "Elite Minor",
 	Class = "npc_iv04_hr_elite_minor",
